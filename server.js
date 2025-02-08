ecdcd4843de5a804c25c4cefc4dc2fb1455e10fd
gentille <tuyisengegentille80@gmail.com>
2025-02-08 13:31:55 +0200

    callback_function

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-import express from "express"
-app=express();
-app.use(expess());
+import express from "express";
+const app = express();
+app.use(express.json()); 
+app.get('/', (req, res) => {
+    res.send('Server started');
+});
+app.listen(4000, () => {
+    console.log("Server is running at http://localhost:4000");
+});
